79ef5372637e0becef9f09c871ea0d255e20c3c7
crystal23733 <48302668+crystal23733@users.noreply.github.com>
2022-05-01 02:08:15 +0900

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -4,16 +4,24 @@ const messageRow = document.querySelector(".message-row");
 
 const replyForm = document.querySelector(".reply");
 const replyInput = document.querySelector(".reply input");
-const replyButton = document.querySelector("reply button");
+
+let newReplys =[];
+
+const NEWREPLYS_KEY = "newReplys";
+
+function saveNewReplys(){
+    localStorage.setItem(NEWREPLYS_KEY, JSON.stringify(newReplys));
+}
 
 function replyEnter(event){
     event.preventDefault();
-    const newReplyInput = replyInput.value;
+    const newReply = replyInput.value;
     replyInput.value = "";
-    myChatBubble(newReplyInput);
+    myChatBubble(newReply);
+    saveNewReplys();
 }
 
-function myChatBubble(newReplyInput){
+function myChatBubble(newReply){
     const messageRowOwnDiv = document.createElement("div");
     mainChatScreen.appendChild(messageRowOwnDiv);
     messageRowOwnDiv.classList.add("my-chat", "message-row", "message-row--own");
@@ -29,8 +37,9 @@ function myChatBubble(newReplyInput){
     const messageTime = document.createElement("span");
     messageInfo.appendChild(messageTime);
     messageTime.classList.add("message__time");
-    messageBubble.textContent = newReplyInput;
+    messageBubble.textContent = newReply;
     getChatClock(messageTime);
+    newReplys.push(newReply);
 }
 
 function getChatClock(messageTime){
@@ -40,4 +49,11 @@ function getChatClock(messageTime){
     messageTime.textContent = `${hours}:${minutes}`;
 }
 
+const savedNewReplys = localStorage.getItem(NEWREPLYS_KEY);
+
 replyForm.addEventListener("submit", replyEnter);
+
+if(savedNewReplys !== null){
+    const parsedNewReplys = JSON.parse(savedNewReplys);
+    parsedNewReplys.forEach(myChatBubble);
+}
